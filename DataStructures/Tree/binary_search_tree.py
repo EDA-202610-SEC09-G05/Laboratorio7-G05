75d061cc7e53cb2ca33a72cdfab1364418fa88f5
aninoh <a.ninoh@uniandes.edu.co>
2026-04-10 18:08:57 -0500

    get + size

--- DataStructures/Tree/binary_search_tree.py
+++ DataStructures/Tree/binary_search_tree.py
@@ -12,11 +12,30 @@ def insert_node(root, key, value):
     return root
 
 def put(my_bst, key, value):
-    """
-    Agrega una nueva pareja llave-valor al árbol binario de búsqueda.
-    Si la llave ya existe, se actualiza su valor.
-    """
+    
     my_bst["root"] = insert_node(my_bst["root"], key, value)
     return my_bst
 
+def get(my_bst, key):
 
+    node = get_node(my_bst['root'], key)
+    
+    return node['value'] if node else None
+
+def get_node(root, key):
+    
+    if root is None or root['key'] == key:
+        return root
+    
+    if key < root['key']:
+        return get_node(root['left'], key)
+    
+    return get_node(root['right'], key)
+
+def size(my_bst):
+    return size_tree(my_bst['root'])
+
+def size_tree(root):
+    if root is None:
+        return 0
+    return root['size']
