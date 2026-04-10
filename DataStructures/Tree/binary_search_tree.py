6a83f1f6b6ac6557bc79f3290ed955af3607cb63
aninoh <a.ninoh@uniandes.edu.co>
2026-04-10 18:02:07 -0500

    insert

--- DataStructures/Tree/binary_search_tree.py
+++ DataStructures/Tree/binary_search_tree.py
@@ -1,3 +1,16 @@
+def insert_node(root, key, value):
+    if root is None:
+        return bst_node(key, value)
+    
+    if key == root.key:
+        root.value = value
+    elif key < root.key:
+        root.left = insert_node(root.left, key, value)
+    else:
+        root.right = insert_node(root.right, key, value)
+    
+    return root
+
 def put(my_bst, key, value):
     """
     Agrega una nueva pareja llave-valor al árbol binario de búsqueda.
@@ -5,3 +18,5 @@ def put(my_bst, key, value):
     """
     my_bst["root"] = insert_node(my_bst["root"], key, value)
     return my_bst
+
+
