ec4aec337c6dfddc8168e7c638d295f1688f40eb
Roxana149 <r.soto@uniandes.edu.co>
2026-04-16 23:31:54 -0500

    cambios binary_search_tree

--- DataStructures/Tree/binary_search_tree.py
+++ DataStructures/Tree/binary_search_tree.py
@@ -1,41 +1,55 @@
+from DataStructures.Tree import bst_node as bn
+
+def default_compare(key1, key2):
+    if key1 == key2:
+        return 0
+    elif key1 < key2:
+        return -1
+    else:
+        return 1
+    
 def insert_node(root, key, value):
     if root is None:
-        return bst_node(key, value)
-    
-    if key == root.key:
-        root.value = value
-    elif key < root.key:
-        root.left = insert_node(root.left, key, value)
+        return bn.new_node(key, value)
+
+    cmp = default_compare(key, root['key'])
+
+    if cmp == 0:
+        root['value'] = value
+    elif cmp < 0:
+        root['left'] = insert_node(root['left'], key, value)
     else:
-        root.right = insert_node(root.right, key, value)
-    
+        root['right'] = insert_node(root['right'], key, value)
+
+    root['size'] = 1 + size_tree(root['left']) + size_tree(root['right'])
     return root
 
 def put(my_bst, key, value):
-    
-    my_bst["root"] = insert_node(my_bst["root"], key, value)
+    my_bst['root'] = insert_node(my_bst['root'], key, value)
     return my_bst
 
-def get(my_bst, key):
-
-    node = get_node(my_bst['root'], key)
-    
-    return node['value'] if node else None
-
 def get_node(root, key):
-    
-    if root is None or root['key'] == key:
-        return root
-    
-    if key < root['key']:
-        return get_node(root['left'], key)
-    
-    return get_node(root['right'], key)
+    if root is None:
+        return None
 
-def size(my_bst):
-    return size_tree(my_bst['root'])
+    cmp = default_compare(key, root['key'])
+
+    if cmp == 0:
+        return root
+    elif cmp < 0:
+        return get_node(root['left'], key)
+    else:
+        return get_node(root['right'], key)
+    
+def get(my_bst, key):
+    node = get_node(my_bst['root'], key)
+    return node['value'] if node else None
 
 def size_tree(root):
     if root is None:
         return 0
     return root['size']
+
+def size(my_bst):
+    return size_tree(my_bst['root'])
+
